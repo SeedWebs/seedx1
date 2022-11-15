c00e0252edbb07efbda363785b7eb3e42739596c
Menn <m@seedwebs.com>
2022-11-15 10:45:00 +0700

    Remove unused css

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head>
-        <link rel='stylesheet' href='/css/desktop.css?v1' media='(min-width: 1024px)' />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
